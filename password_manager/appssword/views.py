1d37cb6540eb1a45cec42a0826f0a91b2d438c39
Inosthful <inosthful@gmail.com>
2024-01-09 16:11:00 +0100

    path coté app good

--- password_manager/appssword/views.py
+++ password_manager/appssword/views.py
@@ -4,7 +4,7 @@ from .forms import SiteForm
 
 def list_sites(request):
     sites = Site.objects.all()
-    return render(request, 'list_sites.html', {'sites': sites})
+    return render(request, 'sites/list_sites.html', {'sites': sites})
 
 def add_site(request):
     if request.method == 'POST':
@@ -14,7 +14,7 @@ def add_site(request):
             return redirect('liste_sites')
     else:
         form = SiteForm()
-    return render(request, 'add_site.html', {'form': form})
+    return render(request, 'sites/add_site.html', {'form': form})
 
 def change_site(request, site_id):
     site = get_object_or_404(Site, pk=site_id)
@@ -25,11 +25,11 @@ def change_site(request, site_id):
             return redirect('liste_sites')
     else:
         form = SiteForm(instance=site)
-    return render(request, 'change_site.html', {'form': form, 'site': site})
+    return render(request, 'sites/change_site.html', {'form': form, 'site': site})
 
 def suppress_site(request, site_id):
     site = get_object_or_404(Site, pk=site_id)
     if request.method == 'POST':
         site.delete()
         return redirect('liste_sites')
-    return render(request, 'suppress_site.html', {'site': site})
+    return render(request, 'sites/suppress_site.html', {'site': site})
