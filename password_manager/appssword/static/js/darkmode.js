7072c8e0fb1981acc711e9c685d530464d81f5ee
JokerPoix <hauteville.raphael@gmail.com>
2024-01-14 17:28:11 +0100

    dark-mode done

--- password_manager/appssword/static/js/darkmode.js
+++ password_manager/appssword/static/js/darkmode.js
@@ -30,11 +30,18 @@
 // });
 $(document).ready(function() {
     // Fonction pour obtenir l'état actuel du mode
+   
     function getDarkMode() {
+        
         $.get('/dark-mode/', function(data) {
+            const darkmodeButton = document.getElementById("toggleDarkMode")
             if (data.dark_mode) {
+                document.body.setAttribute("data-theme", "dark")
+                darkmodeButton.innerHTML = "Dark Mode: ON"
                 console.log("mode sombre")
             } else {
+                document.body.setAttribute("data-theme", "light")
+                darkmodeButton.innerHTML = "Dark Mode: OFF"
                 console.log("mode clair")
             }
         });
